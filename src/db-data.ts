fc8ea31317a6ba9765410080f2b55412ce77e29c
Javier Caparo <javier.caparo@gmail.com>
2020-07-07 21:27:01 -0500

    more entrepreneurs and remove unused spec tests

--- src/db-data.ts
+++ src/db-data.ts
@@ -89,5 +89,25 @@ export const ELEMENT_DATA: EntrepreneurElement[] = [
         description: `Cafe Arabico de Huancabamba - Oxapampa, Variedad: Catimor - Caturra ; Proceso: Lavado ; Tostado: Medio; Perfil en taza: aroma, cítrico - canela, cuerpo medio, acidez suave , sabor caramelo - chocolate `,
         fanpage: ''
     },
+    {
+        id: 9,
+        name: 'Pintamos y Ayudamos',
+        owner: 'Maritza Baca',
+        phone: '997897019',
+        category: 'Arte',
+        symbol: './assets/images/pintamos_y_ayudamos-symbol.jpg',
+        description: `Rogelio es artesano y elabora hermosas alcancías. Por la pandemia hoy cuida a sus hijos y necesita también mantener a su familia. Esta página sirve para ayudarlo, comprando alcancías de yeso para pintar con témperas y otras pinturas. Elige tu modelo!`,
+        fanpage: 'https://www.facebook.com/pinto.ahorro.ayudo/'
+    },
+    {
+        id: 10,
+        name: 'Mesa 24',
+        owner: 'Carlos Callirgos',
+        phone: '989032616',
+        category: 'App Gastronomica',
+        symbol: './assets/images/mesa24_7-symbol.png',
+        description: `Reserva facil. Disfruta mas. Reservas Online en los mejores restaurantes del Peru. reserva tu mesa gratis`,
+        fanpage: 'https://m.mesa247.pe/#/'
+    },
 
 ];
